8ce3061c729469b00dfeb1e8396fcd07c0618fb0
Ebuka <ebuka2264@yahoo.com>
2023-12-20 15:49:36 +0100

    feat: clean up

--- main.py
+++ main.py
@@ -6,34 +6,34 @@ chain = Chain()
 seed = "elevator notable sword phrase doctor tell nice arrow melody judge bleak worth"
 
 # generate phrase
-#phrase = chain.generate_phrase("password")
-#print(phrase)
+phrase = chain.generate_phrase("password")
+print("phrase: ",phrase)
 
 # generate wallet address
-# addr = chain.get_wallet_address(phrase)
-# print(addr)
+addr = chain.get_wallet_address(phrase)
+print(f"Address: {addr}")
 
 # get balance of address
-# balance = chain.get_balance(addr);
-# print(balance)
+balance = chain.get_balance(addr);
+print(f"Balance of {addr} is {balance}")
 
 # get block hash
-#block_hash = chain.get_block_hash(66335838)
-#print(block_hash)
+block_hash = chain.get_block_hash(66335838)
+print(f"Block hash: {block_hash}")
 
 # get block number
-#block_number = chain.get_block_number("0xa3c2290488a124a82a7fd57d7c54edaebd6b214d2330e6510291b40fc8887c9a")
-#print(block_number)
+block_number = chain.get_block_number("0xa3c2290488a124a82a7fd57d7c54edaebd6b214d2330e6510291b40fc8887c9a")
+print(f"Block number : {block_number}")
 
 # sign a statement with your keys
 statement = "A legal binding contract ......"
-signature = chain.sign(seed, statement)
-print(signature)
+signature = chain.sign(phrase, statement)
+print(f"Signature: {signature}")
 
 # get public keys
-kpub = chain.get_public_key(seed)
-print(kpub)
+kpub = chain.get_public_key(phrase)
+print(f"Public key: {kpub}")
 
 # verify signed statement
 verified = chain.verify_sig(signature,statement,kpub)
-print(verified)
+print(f"Verification was {verified}")
